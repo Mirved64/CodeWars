332edc4c1e206fb2c9c8cd1ff746a1c31660d263
Mirved64 <medmixal@yahoo.com>
2024-01-30 15:35:50 +0400

    feat: Invert values

---
+++ 8kyu/Invert-values.ts
@@ -0,0 +1,8 @@
+// Given a set of numbers, return the additive inverse of each.
+// Each positive becomes negatives, and the negatives become positives.
+//
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+export const invert = (array: number[]): number[] => array.length === 0 ? [] : array.map((el) => el * (-1))
