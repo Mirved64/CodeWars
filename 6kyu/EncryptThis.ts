9cb3a74a8d3e458602fcd08d0a1bc9842548a40c
Mirved64 <medmixal@yahoo.com>
2024-02-01 15:53:02 +0400

    feat: Encrypt this!

---
+++ 6kyu/EncryptThis.ts
@@ -0,0 +1,18 @@
+// Description:
+//   Encrypt this!
+//
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+//
+//   Your message is a string containing space separated words.
+//   You need to encrypt each word in the message using the following rules:
+//   The first letter must be converted to its ASCII code.
+//   The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+// Examples:
+//   encryptThis "Hello" == "72olle"
+// encryptThis "good" == "103doo"
+// encryptThis "hello world" == "104olle 119drlo"
+
+export const encryptThis = (str: string): string =>
+  str.replace(/\b\w(\w?)(\w*?)(\w?)\b/g, (word, a, mid, b) =>
+  word.charCodeAt(0) + b + mid + a)
