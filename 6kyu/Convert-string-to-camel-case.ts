63a293d4d767d23dc3607a1b8cacfbd277353be3
Mirved64 <medmixal@yahoo.com>
2024-03-05 11:41:17 +0400

    feat: Convert string to camel case

---
+++ 6kyu/Convert-string-to-camel-case.ts
@@ -0,0 +1,19 @@
+// casing. The first word within the output should be capitalized only if
+// the original word was capitalized (known as Upper Camel Case, also often
+// referred to as Pascal case). The next words should be always capitalized.
+//
+//   Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+//
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+//
+// "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+function toCamelCase(str: string): string {
+  return str.split(/[-|_]/)
+    .map((el, index) =>
+      index !== 0
+        ? el.split('').map((val, currentIndex) => currentIndex === 0 ? val.toUpperCase() : val).join('')
+        : el
+    ).join('')
+}
